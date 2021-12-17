02f54cb5df92a4c070aa07791a5e842f08bd0d9e
fredrischter <fredrischter@gmail.cmo>
2021-12-17 02:25:29 +0100

    App implementation. Compiler code level fix. Read-me improvement.

--- README.md
+++ README.md
@@ -2,16 +2,24 @@
 
 Simple sudoku file validation application.
 
+Data structure problem was treated as exception, as it is not expected to happen. Logical "error" like having repeated number was treated as information added to a ValidationResult, as is expected to happen during the game.
+
+As I like data centered approach, I've left some validation withing the data class. Repository responsible for healthy retrieval. Service responsible for game logic / validation as a top level layer.
+
+Packaging includes dependencies. Used some apache commons libs to simplify code.
+
 # Requirements
 
-Maven
-Java
+Maven 3
+Java 14
 
 # Instruction
 
-To run, execute validate.bat providing file address as parameter. Example:
+To run, execute validate.bat providing file address as parameter. Examples:
 
 validate.bat src\test\resources\valid.txt
+validate.bat src\test\resources\validEmpty.txt
+validate.bat src\test\resources\sevenRows.txt
 
 File should be in the following format:
 
@@ -40,3 +48,9 @@ P05 - Unwnown error.
 L01 - Invalid row, having repeated number. Row X, repeated number Y.
 L02 - Invalid column, having repeated number. Column X, repeated number Y.
 L03 - Invalid box, having repeated number. Box X,Y, repeated number X.
+
+# To do
+
+- To reject unsolvable sudokus.
+- Replace tabs with spaces / format everything with agreed format.
+- To test complexity.
--- src/main/java/com/test/sudoku/App.java
+++ src/main/java/com/test/sudoku/App.java
@@ -1,13 +1,53 @@
 package com.test.sudoku;
 
-/**
- * Hello world!
- *
- */
+import java.io.File;
+
+import com.test.sudoku.model.Result;
+import com.test.sudoku.model.SudokuImmutableData;
+import com.test.sudoku.model.exception.SudokuFileNotFoundException;
+import com.test.sudoku.model.exception.SudokuNotExpectedColumnCountException;
+import com.test.sudoku.model.exception.SudokuNotExpectedRowCountException;
+import com.test.sudoku.repository.SudokuFileRepository;
+import com.test.sudoku.service.SudokuValidationService;
+
 public class App 
 {
-    public static void main( String[] args )
+	
+	private static final int RETURN_MISSING_PARAMETER = 1;
+	private static final int RETURN_INVALID = 2;
+	private static final int RETURN_FILE_NOT_FOUND = 3;
+	private static final int RETURN_INVALID_ROW_COUNT = 4;
+	private static final int RETURN_INVALID_COLUMN_COUNT = 5;
+    private static final int RETURN_UNKNOWN_ERROR = 6;
+
+	public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+		if (args.length == 0) {
+			System.out.println("P01 - Missing parameter - file.");
+			System.exit(RETURN_MISSING_PARAMETER);
+		}
+    	SudokuFileRepository sudokuFileRepository = new SudokuFileRepository();
+    	SudokuValidationService sudokuValidationService = new SudokuValidationService();
+    	try {
+			SudokuImmutableData sudokuImmutableData = sudokuFileRepository.loadFromFile(new File(args[0]));
+			Result result = sudokuValidationService.validate(sudokuImmutableData);
+			
+			if (!result.isValid()) {
+				System.out.print(result.getMessages());
+				System.exit(RETURN_INVALID);
+			}
+		} catch (SudokuFileNotFoundException e) {
+			System.out.print("P02 - File not found.");
+			System.exit(RETURN_FILE_NOT_FOUND);
+		} catch (SudokuNotExpectedRowCountException e) {
+			System.out.print("P03 - Doesn't contain the expected rows count. "+e.getCount()+" rows found.");
+			System.exit(RETURN_INVALID_ROW_COUNT);
+		} catch (SudokuNotExpectedColumnCountException e) {
+			System.out.print("P04 - Row doesn't contain the expected columns count. "+e.getCount()+" columns found on the row "+e.getIndex()+".");
+			System.exit(RETURN_INVALID_COLUMN_COUNT);
+		} catch (Exception e) {
+			System.out.print("P05 - Unknown error. "+e.getMessage());
+			System.exit(RETURN_UNKNOWN_ERROR);
+		}
     }
 }
